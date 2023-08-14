66086405eb62fb894d1c58b88a6880696f122bd3
KoahlDevOfficial <com.zenon.dev@outlook.com>
2023-08-14 13:00:34 +0100

    Update DevelopmentLevel.hx

--- source/levels/DevelopmentLevel.hx
+++ source/levels/DevelopmentLevel.hx
@@ -1,17 +1,59 @@
 package levels;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.input.keyboard.FlxKey;
+import flixel.util.FlxColor;
 
 class DevelopmentLevel extends FlxState
 {
-	override public function create()
+	// * Player Controller Configuration
+	var GroundLevel = 700;
+	var LeftWall = 0;
+	var RightWall = FlxG.width;
+	var SpawnCoords = [0, 600]; // X, Y
+
+	// * Assets
+	var Character:FlxSprite;
+
+	public override function create()
 	{
-		super.create();
+		Character = new FlxSprite();
+		Character.makeGraphic(50, 50, FlxColor.WHITE);
+		Character.x = SpawnCoords[0];
+		Character.y = SpawnCoords[1];
+		add(Character);
 	}
 
-	override public function update(elapsed:Float)
+	public override function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		// Physics and Movement Calculation :D
+		locomotionCalculation();
+	}
+
+	public function locomotionCalculation()
+	{
+		if (Character.y != GroundLevel)
+		{
+			Character.y += 2;
+		}
+
+		if (Character.y > GroundLevel)
+		{
+			Character.y = 700;
+		}
+
+		if (FlxG.keys.anyPressed([FlxKey.D, FlxKey.RIGHT]))
+		{
+			Character.x += 10;
+		}
+
+		if (FlxG.keys.anyPressed([FlxKey.LEFT, FlxKey.A]))
+		{
+			Character.x -= 10;
+		}
 	}
 }
